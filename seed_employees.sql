e5c71b9831fd54533bb84dfc366b3b658fb1084a
Paolo Paci <paolopci@yahoo.it>
2026-03-06 10:12:00 +0100

    update .sql 100 items

--- seed_employees.sql
+++ seed_employees.sql
@@ -7,33 +7,96 @@ SET XACT_ABORT ON;
 BEGIN TRANSACTION;
 
 DECLARE @InsertedEmployees INT = 0;
+DECLARE @UpdatedEmployees INT = 0;
 DECLARE @InsertedReviews INT = 0;
+DECLARE @UpdatedReviews INT = 0;
 
-;WITH SeedData (FirstName, LastName, Email) AS
+;WITH Numbers AS
+(
+    SELECT TOP (100)
+        ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS N
+    FROM sys.all_objects
+),
+FirstNames AS
 (
     SELECT *
     FROM (VALUES
-        ('Luca', 'Rossi', 'luca.rossi@azienda.it'),
-        ('Marco', 'Bianchi', 'marco.bianchi@azienda.it'),
-        ('Giulia', 'Romano', 'giulia.romano@azienda.it'),
-        ('Francesca', 'Greco', 'francesca.greco@azienda.it'),
-        ('Alessandro', 'Bruno', 'alessandro.bruno@azienda.it'),
-        ('Chiara', 'Gallo', 'chiara.gallo@azienda.it'),
-        ('Matteo', 'Conti', 'matteo.conti@azienda.it'),
-        ('Sara', 'DeLuca', 'sara.deluca@azienda.it'),
-        ('Davide', 'Mancini', 'davide.mancini@azienda.it'),
-        ('Elena', 'Costa', 'elena.costa@azienda.it'),
-        ('Simone', 'Giordano', 'simone.giordano@azienda.it'),
-        ('Federica', 'Rinaldi', 'federica.rinaldi@azienda.it'),
-        ('Andrea', 'Moretti', 'andrea.moretti@azienda.it'),
-        ('Valentina', 'Barbieri', 'valentina.barbieri@azienda.it'),
-        ('Stefano', 'Ferrari', 'stefano.ferrari@azienda.it'),
-        ('Martina', 'Marini', 'martina.marini@azienda.it'),
-        ('Gabriele', 'Lombardi', 'gabriele.lombardi@azienda.it'),
-        ('Noemi', 'Parisi', 'noemi.parisi@azienda.it'),
-        ('Riccardo', 'Caruso', 'riccardo.caruso@azienda.it'),
-        ('Alessia', 'Santoro', 'alessia.santoro@azienda.it')
-    ) AS v (FirstName, LastName, Email)
+        (1, 'Luca'), (2, 'Marco'), (3, 'Giulia'), (4, 'Francesca'), (5, 'Alessandro'),
+        (6, 'Chiara'), (7, 'Matteo'), (8, 'Sara'), (9, 'Davide'), (10, 'Elena'),
+        (11, 'Simone'), (12, 'Federica'), (13, 'Andrea'), (14, 'Valentina'), (15, 'Stefano'),
+        (16, 'Martina'), (17, 'Gabriele'), (18, 'Noemi'), (19, 'Riccardo'), (20, 'Alessia')
+    ) AS f(Id, Value)
+),
+LastNames AS
+(
+    SELECT *
+    FROM (VALUES
+        (1, 'Rossi'), (2, 'Bianchi'), (3, 'Romano'), (4, 'Greco'), (5, 'Bruno'),
+        (6, 'Gallo'), (7, 'Conti'), (8, 'DeLuca'), (9, 'Mancini'), (10, 'Costa'),
+        (11, 'Giordano'), (12, 'Rinaldi'), (13, 'Moretti'), (14, 'Barbieri'), (15, 'Ferrari'),
+        (16, 'Marini'), (17, 'Lombardi'), (18, 'Parisi'), (19, 'Caruso'), (20, 'Santoro'),
+        (21, 'Esposito'), (22, 'Ricci'), (23, 'Leone'), (24, 'Pellegrini'), (25, 'Serra')
+    ) AS l(Id, Value)
+),
+SeedData AS
+(
+    SELECT
+        n.N,
+        fn.Value AS FirstName,
+        ln.Value AS LastName,
+        CONCAT('employee', RIGHT(CONCAT('000', CAST(n.N AS varchar(3))), 3), '@azienda.it') AS Email
+    FROM Numbers n
+    INNER JOIN FirstNames fn ON fn.Id = ((n.N - 1) % 20) + 1
+    INNER JOIN LastNames ln ON ln.Id = ((n.N - 1) % 25) + 1
+)
+UPDATE e
+SET
+    e.FirstName = s.FirstName,
+    e.LastName = s.LastName
+FROM dbo.Employee e
+INNER JOIN SeedData s ON s.Email = e.Email
+WHERE e.FirstName <> s.FirstName
+   OR e.LastName <> s.LastName;
+
+SET @UpdatedEmployees = @@ROWCOUNT;
+
+;WITH Numbers AS
+(
+    SELECT TOP (100)
+        ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS N
+    FROM sys.all_objects
+),
+FirstNames AS
+(
+    SELECT *
+    FROM (VALUES
+        (1, 'Luca'), (2, 'Marco'), (3, 'Giulia'), (4, 'Francesca'), (5, 'Alessandro'),
+        (6, 'Chiara'), (7, 'Matteo'), (8, 'Sara'), (9, 'Davide'), (10, 'Elena'),
+        (11, 'Simone'), (12, 'Federica'), (13, 'Andrea'), (14, 'Valentina'), (15, 'Stefano'),
+        (16, 'Martina'), (17, 'Gabriele'), (18, 'Noemi'), (19, 'Riccardo'), (20, 'Alessia')
+    ) AS f(Id, Value)
+),
+LastNames AS
+(
+    SELECT *
+    FROM (VALUES
+        (1, 'Rossi'), (2, 'Bianchi'), (3, 'Romano'), (4, 'Greco'), (5, 'Bruno'),
+        (6, 'Gallo'), (7, 'Conti'), (8, 'DeLuca'), (9, 'Mancini'), (10, 'Costa'),
+        (11, 'Giordano'), (12, 'Rinaldi'), (13, 'Moretti'), (14, 'Barbieri'), (15, 'Ferrari'),
+        (16, 'Marini'), (17, 'Lombardi'), (18, 'Parisi'), (19, 'Caruso'), (20, 'Santoro'),
+        (21, 'Esposito'), (22, 'Ricci'), (23, 'Leone'), (24, 'Pellegrini'), (25, 'Serra')
+    ) AS l(Id, Value)
+),
+SeedData AS
+(
+    SELECT
+        n.N,
+        fn.Value AS FirstName,
+        ln.Value AS LastName,
+        CONCAT('employee', RIGHT(CONCAT('000', CAST(n.N AS varchar(3))), 3), '@azienda.it') AS Email
+    FROM Numbers n
+    INNER JOIN FirstNames fn ON fn.Id = ((n.N - 1) % 20) + 1
+    INNER JOIN LastNames ln ON ln.Id = ((n.N - 1) % 25) + 1
 )
 INSERT INTO dbo.Employee (FirstName, LastName, Email)
 SELECT s.FirstName, s.LastName, s.Email
@@ -47,48 +110,61 @@ WHERE NOT EXISTS
 
 SET @InsertedEmployees = @@ROWCOUNT;
 
-;WITH SeedReviews (EmployeeEmail, Rate, Comment) AS
+;WITH SeedReviews AS
 (
-    SELECT *
-    FROM (VALUES
-        ('luca.rossi@azienda.it', 5, 'Professionale e puntuale.'),
-        ('marco.bianchi@azienda.it', 4, 'Ottima capacita di problem solving.'),
-        ('giulia.romano@azienda.it', 5, 'Collaborativa e precisa.'),
-        ('francesca.greco@azienda.it', 4, 'Consegne sempre nei tempi.'),
-        ('alessandro.bruno@azienda.it', 5, 'Leadership tecnica eccellente.'),
-        ('chiara.gallo@azienda.it', 4, 'Comunicazione chiara con il team.'),
-        ('matteo.conti@azienda.it', 5, 'Qualita del codice molto alta.'),
-        ('sara.deluca@azienda.it', 4, 'Affidabile nelle attivita critiche.'),
-        ('davide.mancini@azienda.it', 5, 'Proattivo nel miglioramento continuo.'),
-        ('elena.costa@azienda.it', 4, 'Buona gestione delle priorita.'),
-        ('simone.giordano@azienda.it', 5, 'Supporta efficacemente i colleghi.'),
-        ('federica.rinaldi@azienda.it', 4, 'Attenta ai dettagli funzionali.'),
-        ('andrea.moretti@azienda.it', 5, 'Eccellente autonomia operativa.'),
-        ('valentina.barbieri@azienda.it', 4, 'Ottimo contributo alle retrospettive.'),
-        ('stefano.ferrari@azienda.it', 5, 'Performance costante e solida.'),
-        ('martina.marini@azienda.it', 4, 'Buona qualita della documentazione.'),
-        ('gabriele.lombardi@azienda.it', 5, 'Grande affidabilita in produzione.'),
-        ('noemi.parisi@azienda.it', 4, 'Approccio pragmatico ai problemi.'),
-        ('riccardo.caruso@azienda.it', 5, 'Spirito di iniziativa elevato.'),
-        ('alessia.santoro@azienda.it', 4, 'Ottima collaborazione cross-team.')
-    ) AS v (EmployeeEmail, Rate, Comment)
+    SELECT
+        e.Id AS EmployeeId,
+        CASE WHEN e.Id % 2 = 0 THEN 4 ELSE 5 END AS Rate,
+        CONCAT('Valutazione annuale seed per ', e.FirstName, ' ', e.LastName, ' (', e.Email, ').') AS Comment
+    FROM dbo.Employee e
+    WHERE e.Email LIKE 'employee___@azienda.it'
+),
+ReviewTarget AS
+(
+    SELECT
+        sr.EmployeeId,
+        sr.Rate,
+        sr.Comment,
+        (
+            SELECT MIN(r.Id)
+            FROM dbo.Review r
+            WHERE r.EmployeeId = sr.EmployeeId
+        ) AS ExistingReviewId
+    FROM SeedReviews sr
+)
+UPDATE r
+SET
+    r.Rate = rt.Rate,
+    r.Comment = rt.Comment
+FROM dbo.Review r
+INNER JOIN ReviewTarget rt ON r.Id = rt.ExistingReviewId
+WHERE r.Rate <> rt.Rate
+   OR r.Comment <> rt.Comment;
+
+SET @UpdatedReviews = @@ROWCOUNT;
+
+;WITH SeedReviews AS
+(
+    SELECT
+        e.Id AS EmployeeId,
+        CASE WHEN e.Id % 2 = 0 THEN 4 ELSE 5 END AS Rate,
+        CONCAT('Valutazione annuale seed per ', e.FirstName, ' ', e.LastName, ' (', e.Email, ').') AS Comment
+    FROM dbo.Employee e
+    WHERE e.Email LIKE 'employee___@azienda.it'
 )
 INSERT INTO dbo.Review (Rate, Comment, EmployeeId)
-SELECT sr.Rate, sr.Comment, e.Id
+SELECT sr.Rate, sr.Comment, sr.EmployeeId
 FROM SeedReviews sr
-INNER JOIN dbo.Employee e ON e.Email = sr.EmployeeEmail
 WHERE NOT EXISTS
 (
     SELECT 1
     FROM dbo.Review r
-    WHERE r.EmployeeId = e.Id
-      AND r.Rate = sr.Rate
-      AND r.Comment = sr.Comment
+    WHERE r.EmployeeId = sr.EmployeeId
 );
 
 SET @InsertedReviews = @@ROWCOUNT;
 
 COMMIT TRANSACTION;
 
-PRINT CONCAT('Inserimento Employee completato. Nuove righe: ', @InsertedEmployees);
-PRINT CONCAT('Inserimento Review completato. Nuove righe: ', @InsertedReviews);
+PRINT CONCAT('Employee aggiornati: ', @UpdatedEmployees, ', inseriti: ', @InsertedEmployees);
+PRINT CONCAT('Review aggiornate: ', @UpdatedReviews, ', inserite: ', @InsertedReviews);
