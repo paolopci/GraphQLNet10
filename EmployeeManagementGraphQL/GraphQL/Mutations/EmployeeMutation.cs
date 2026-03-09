cf371846add369f3388b9f06b1f3c7bb5c64beb0
Paolo Paci <paolopci@yahoo.it>
2026-03-09 11:41:15 +0100

    complete: implement Update and delete Employee with GraphQL

--- EmployeeManagementGraphQL/GraphQL/Mutations/EmployeeMutation.cs
+++ EmployeeManagementGraphQL/GraphQL/Mutations/EmployeeMutation.cs
@@ -36,5 +36,60 @@ public class EmployeeMutation : ObjectGraphType
 
                 return employeeRepository.AddEmployee(employee);
             });
+
+        Field<EmployeeGraphType>(
+            "updateEmployee",
+            "Update an existing employee",
+            arguments: new QueryArguments(
+                new QueryArgument<NonNullGraphType<IntGraphType>>
+                {
+                    Name = "id",
+                    Description = "Employee id to update."
+                },
+                new QueryArgument<NonNullGraphType<EmployeeInputType>>
+                {
+                    Name = "input",
+                    Description = "Employee data to update."
+                }),
+            resolve: context =>
+            {
+                var id = context.GetArgument<int>("id");
+                var input = context.GetArgument<Employee>("input");
+                if (input is null)
+                {
+                    throw new ExecutionError("The 'input' argument is required.");
+                }
+
+                var employee = new Employee
+                {
+                    FirstName = input.FirstName,
+                    LastName = input.LastName,
+                    Email = input.Email
+                };
+
+                return employeeRepository.UpdateEmployee(id, employee);
+            });
+
+        Field<NonNullGraphType<BooleanGraphType>>(
+            "deleteEmployee",
+            "Delete an employee by id",
+            arguments: new QueryArguments(
+                new QueryArgument<NonNullGraphType<IntGraphType>>
+                {
+                    Name = "id",
+                    Description = "Employee id to delete."
+                }),
+            resolve: context =>
+            {
+                var id = context.GetArgument<int>("id");
+                var employee = employeeRepository.GetEmployeeById(id);
+                if (employee is null)
+                {
+                    return false;
+                }
+
+                employeeRepository.DeleteEmployee(id);
+                return true;
+            });
     }
 }
